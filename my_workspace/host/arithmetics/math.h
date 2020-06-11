9b9c7d19e8fd51722a80f0b57fa6ac8010663c11
PericR <robertperic@outlook.com>
2020-06-11 21:19:05 +0200

    Init commit

---
+++ my_workspace/host/arithmetics/math.h
@@ -0,0 +1,16 @@
+/*
+ * math.h
+ *
+ *  Created on: May 11, 2020
+ *      Author: Robert
+ */
+
+#ifndef MATH_H_
+#define MATH_H_
+
+int addition(int a, int b);
+int subtraction(int a, int b);
+long long int multiplication(int a, int b);
+float division(int a, int b);
+
+#endif /* MATH_H_ */
