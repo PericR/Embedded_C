d2c4d9f4a114eca195f47801ea76b7e91d45ab15
PericR <robertperic@outlook.com>
2020-07-06 18:14:30 +0200

    PeripClock EN/DI for GPIO, I2C, SPI, USART, SYSCFG

--- MCU drivers/target/stm32f446_drivers/Debug/Src/main.d
+++ MCU drivers/target/stm32f446_drivers/Debug/Src/main.d
@@ -1,1 +1,4 @@
-Src/main.o: ../Src/main.c
+Src/main.o: ../Src/main.c \
+ /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/Inc/stm32f446re.h
+
+/home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/Inc/stm32f446re.h:
