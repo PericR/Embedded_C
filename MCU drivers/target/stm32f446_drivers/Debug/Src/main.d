778cfeb1d2fc643adbe6eff44c2cad49bd468f43
PericR <robertperic@outlook.com>
2020-07-06 12:40:13 +0200

    mapping base addresses of busses, periphe and mem

--- MCU drivers/target/stm32f446_drivers/Debug/Src/main.d
+++ MCU drivers/target/stm32f446_drivers/Debug/Src/main.d
@@ -1,4 +1,1 @@
-Src/main.o: ../Src/main.c \
- /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/Inc/stm32f446re.h
-
-/home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/Inc/stm32f446re.h:
+Src/main.o: ../Src/main.c
