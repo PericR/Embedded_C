0c380381a522387f3a25658f7cb73c79cb031821
PericR <robertperic@outlook.com>
2020-07-15 21:15:13 +0200

    GPIO data read and write pins/ports

---
+++ MCU drivers/target/stm32f446_drivers/Debug/drivers/Src/stm32f446re_gpio_driver.d
@@ -0,0 +1,8 @@
+drivers/Src/stm32f446re_gpio_driver.o: \
+ ../drivers/Src/stm32f446re_gpio_driver.c \
+ /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/Inc/stm32f446re_gpio_driver.h \
+ /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/Inc/stm32f446re.h
+
+/home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/Inc/stm32f446re_gpio_driver.h:
+
+/home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/Inc/stm32f446re.h:
