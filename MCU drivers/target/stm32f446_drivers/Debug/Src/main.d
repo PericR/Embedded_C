4c0de4b049e021107d0b0af77d2d89cd29f531d7
PericR <robertperic@outlook.com>
2020-07-20 09:40:09 +0200

    GPIO IRQ handler

--- MCU drivers/target/stm32f446_drivers/Debug/Src/main.d
+++ MCU drivers/target/stm32f446_drivers/Debug/Src/main.d
@@ -1,4 +1,4 @@
-Src/001_led_toggle_external.o: ../Src/001_led_toggle_external.c \
+Src/main.o: ../Src/main.c \
  /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/Inc/stm32f446re.h \
  /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/Inc/stm32f446re_gpio_driver.h \
  /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/Inc/stm32f446re.h
--- MCU drivers/target/stm32f446_drivers/Debug/Src/subdir.mk
+++ MCU drivers/target/stm32f446_drivers/Debug/Src/subdir.mk
@@ -4,24 +4,24 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/001_led_toggle_external.c \
+../Src/main.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/001_led_toggle_external.o \
+./Src/main.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/001_led_toggle_external.d \
+./Src/main.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Src/001_led_toggle_external.o: ../Src/001_led_toggle_external.c
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -I"/home/robert/Projects/Embedded-C/MCU drivers/target/stm32f446_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/001_led_toggle_external.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Src/main.o: ../Src/main.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -I"/home/robert/Projects/Embedded-C/MCU drivers/target/stm32f446_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/main.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 Src/syscalls.o: ../Src/syscalls.c
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -I"/home/robert/Projects/Embedded-C/MCU drivers/target/stm32f446_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/syscalls.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 Src/sysmem.o: ../Src/sysmem.c
