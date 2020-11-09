98f701d754971ee407b1ceacc5730baf5d8cfc03
PericR <robertperic@outlook.com>
2020-11-09 11:27:34 +0100

    peripheral register definition structure for GPIO

--- MCU drivers/target/stm32f446_drivers/Debug/Src/main.d
+++
@@ -1,4 +0,0 @@
-Src/main.o: ../Src/main.c \
- /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h
-
-/home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h:
