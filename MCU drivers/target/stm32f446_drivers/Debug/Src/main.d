24513651268a42877e55564e9bf1f0ffe0722278
PericR <robertperic@outlook.com>
2020-11-10 10:02:16 +0100

    GPIO handle and pin configuration structures added

--- MCU drivers/target/stm32f446_drivers/Debug/Src/main.d
+++ MCU drivers/target/stm32f446_drivers/Debug/Src/main.d
@@ -1,1 +1,4 @@
-Src/main.o: ../Src/main.c
+Src/main.o: ../Src/main.c \
+ /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h
+
+/home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h:
---
+++ MCU drivers/target/stm32f446_drivers/drivers/src/stm32f446_gpio_driver .c
@@ -0,0 +1,8 @@
+/*
+ * stm32f446_gpio_driver .c
+ *
+ *  Created on: Nov 10, 2020
+ *      Author: robert
+ */
+
+#include "stm32f446_gpio_driver.h"
