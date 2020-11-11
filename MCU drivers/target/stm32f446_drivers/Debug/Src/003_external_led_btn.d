77d16bcbd42ec5925cfdb9c1f5b0b9ce7e49b9d6
PericR <robertperic@outlook.com>
2020-11-11 20:07:49 +0100

    Some exercises to test APIs

--- MCU drivers/target/stm32f446_drivers/Debug/Src/003_external_led_btn.d
+++ MCU drivers/target/stm32f446_drivers/Debug/Src/003_external_led_btn.d
@@ -1,4 +1,4 @@
-Src/001_led_toggle.o: ../Src/001_led_toggle.c \
+Src/003_external_led_btn.o: ../Src/003_external_led_btn.c \
  /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h \
  /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446_gpio_driver.h \
  /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h
