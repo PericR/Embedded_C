b940ee1cb9b4b6aa41caffce9b6b1c71e33f839f
PericR <robertperic@outlook.com>
2020-11-11 13:26:35 +0100

    LED_toggle exercise

---
+++ MCU drivers/target/stm32f446_drivers/Debug/Src/001_led_toggle.d
@@ -0,0 +1,10 @@
+Src/001_led_toggle.o: ../Src/001_led_toggle.c \
+ /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h \
+ /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446_gpio_driver.h \
+ /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h
+
+/home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h:
+
+/home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446_gpio_driver.h:
+
+/home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h:
