6b79efcc149fd83f04d03aad00b42aa918cf7983
PericR <robertperic@outlook.com>
2020-12-01 11:07:17 +0100

    SPI configuration structure and handle

--- MCU drivers/target/stm32f446_drivers/drivers/inc/stm32f446_spi_driver.h
+++ MCU drivers/target/stm32f446_drivers/drivers/inc/stm32f446_spi_driver.h
@@ -8,6 +8,29 @@
 #ifndef INC_STM32F446_SPI_DRIVER_H_
 #define INC_STM32F446_SPI_DRIVER_H_
 
+#include "stm32f446.h"
+#include <stdint.h>
+/*
+ *	Configuration structure for SPIx peripheral
+ */
+typedef struct{
+	uint8_t SPI_DeviceMode;					/**/
+	uint8_t SPI_BusConfig;					/**/
+	uint8_t SPI_SclkSpeed;					/**/
+	uint8_t SPI_DFF;						/**/
+	uint8_t SPI_CPOL;						/**/
+	uint8_t SPI_CPHA;						/**/
+	uint8_t SPI_SSM;						/**/
+} SPI_PinConfig_t;
+
+/*
+ *	 This is the handle for SPIx peripheral
+ */
+typedef struct{
+	GPIO_RegDef_t *pSPIx;					/*This holds the base address of the SPI port*/
+	GPIO_PinConfig_t SPI_PinConfig;			/*This holds SPI peripheral configuration settings*/
+} SPI_Handle_t;
+
 
 
 #endif /* INC_STM32F446_SPI_DRIVER_H_ */
