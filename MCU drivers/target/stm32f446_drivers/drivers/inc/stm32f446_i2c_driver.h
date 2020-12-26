920b0e0ea485d2b01e663dc2b3882327953443ff
PericR <robertperic@outlook.com>
2020-12-26 09:28:23 +0100

    I2C source and header files

---
+++ MCU drivers/target/stm32f446_drivers/drivers/inc/stm32f446_i2c_driver.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f446_i2c_driver.h
+ *
+ *  Created on: Dec 26, 2020
+ *      Author: robert
+ */
+
+#ifndef INC_STM32F446_I2C_DRIVER_H_
+#define INC_STM32F446_I2C_DRIVER_H_
+
+
+
+#endif /* INC_STM32F446_I2C_DRIVER_H_ */
---
+++ MCU drivers/target/stm32f446_drivers/drivers/src/stm32f446_i2c_driver.c
@@ -0,0 +1,8 @@
+/*
+ * stm32f446_i2c_driver.c
+ *
+ *  Created on: Dec 26, 2020
+ *      Author: robert
+ */
+
+
