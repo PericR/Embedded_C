c48f6350ce02eb5390760d4cae398f54aa2b8071
PericR <robertperic@outlook.com>
2020-12-28 08:19:43 +0100

    I2C_PeriClockControl implemented

--- MCU drivers/target/stm32f446_drivers/Debug/Src/008_SPI_cmd_handling.d
+++ MCU drivers/target/stm32f446_drivers/Debug/Src/008_SPI_cmd_handling.d
@@ -2,7 +2,8 @@ Src/008_SPI_cmd_handling.o: ../Src/008_SPI_cmd_handling.c \
  /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h \
  /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446_gpio_driver.h \
  /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h \
- /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446_spi_driver.h
+ /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446_spi_driver.h \
+ /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446_i2c_driver.h
 
 /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h:
 
@@ -11,3 +12,5 @@ Src/008_SPI_cmd_handling.o: ../Src/008_SPI_cmd_handling.c \
 /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h:
 
 /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446_spi_driver.h:
+
+/home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446_i2c_driver.h:
