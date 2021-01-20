a4c6789a40f3761e6eb9b5c54ed57287d3b7f648
PericR <robertperic@outlook.com>
2021-01-20 10:27:28 +0100

    I2C finished

--- MCU drivers/target/stm32f446_drivers/Debug/Src/014i2c_slave_tx_string_big.d
+++ MCU drivers/target/stm32f446_drivers/Debug/Src/014i2c_slave_tx_string_big.d
@@ -1,4 +1,4 @@
-Src/012i2c_master_rx_test_IT.o: ../Src/012i2c_master_rx_test_IT.c \
+Src/014i2c_slave_tx_string_big.o: ../Src/014i2c_slave_tx_string_big.c \
  /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h \
  /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446_gpio_driver.h \
  /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h \
--- MCU drivers/target/stm32f446_drivers/Debug/Src/subdir.mk
+++ MCU drivers/target/stm32f446_drivers/Debug/Src/subdir.mk
@@ -4,21 +4,21 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/012i2c_master_rx_test_IT.c \
+../Src/014i2c_slave_tx_string_big.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/012i2c_master_rx_test_IT.o \
+./Src/014i2c_slave_tx_string_big.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/012i2c_master_rx_test_IT.d \
+./Src/014i2c_slave_tx_string_big.d \
 ./Src/sysmem.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Src/012i2c_master_rx_test_IT.o: ../Src/012i2c_master_rx_test_IT.c
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -I"/home/robert/Projects/Embedded-C/MCU drivers/target/stm32f446_drivers/drivers/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/012i2c_master_rx_test_IT.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Src/014i2c_slave_tx_string_big.o: ../Src/014i2c_slave_tx_string_big.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -I"/home/robert/Projects/Embedded-C/MCU drivers/target/stm32f446_drivers/drivers/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/014i2c_slave_tx_string_big.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 Src/sysmem.o: ../Src/sysmem.c
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -I"/home/robert/Projects/Embedded-C/MCU drivers/target/stm32f446_drivers/drivers/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/sysmem.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
