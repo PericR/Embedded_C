f9bde4a1e9fec27208cc8873aaaff55de5c6c0dd
PericR <robertperic@outlook.com>
2021-01-23 21:47:48 +0100

    016 exercise

--- MCU drivers/target/stm32f446_drivers/Debug/Src/016uart_case_test2.d
+++ MCU drivers/target/stm32f446_drivers/Debug/Src/016uart_case_test2.d
@@ -1,4 +1,4 @@
-Src/015uart_tx_test.o: ../Src/015uart_tx_test.c \
+Src/016uart_case_test2.o: ../Src/016uart_case_test2.c \
  /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h \
  /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446_gpio_driver.h \
  /home/robert/Projects/Embedded-C/MCU\ drivers/target/stm32f446_drivers/drivers/inc/stm32f446.h \
--- MCU drivers/target/stm32f446_drivers/Debug/Src/subdir.mk
+++ MCU drivers/target/stm32f446_drivers/Debug/Src/subdir.mk
@@ -4,21 +4,21 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/015uart_tx_test.c \
+../Src/016uart_case_test2.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/015uart_tx_test.o \
+./Src/016uart_case_test2.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/015uart_tx_test.d \
+./Src/016uart_case_test2.d \
 ./Src/sysmem.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Src/015uart_tx_test.o: ../Src/015uart_tx_test.c
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -I"/home/robert/Projects/Embedded-C/MCU drivers/target/stm32f446_drivers/drivers/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/015uart_tx_test.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Src/016uart_case_test2.o: ../Src/016uart_case_test2.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -I"/home/robert/Projects/Embedded-C/MCU drivers/target/stm32f446_drivers/drivers/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/016uart_case_test2.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 Src/sysmem.o: ../Src/sysmem.c
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -I"/home/robert/Projects/Embedded-C/MCU drivers/target/stm32f446_drivers/drivers/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/sysmem.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
