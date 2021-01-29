b72fafaab90f8758ad1b2d1c473d28265f9a311a
PericR <robertperic@outlook.com>
2021-01-29 20:25:01 +0100

    uart2 low lvl inits

--- MCU2/001_UART_all_caps/Core/Src/main.c
+++ MCU2/001_UART_all_caps/Core/Src/main.c
@@ -10,6 +10,7 @@
 
 void SystemClockConfig(void);
 void UART2_Init(void);
+void HAL_UART_MspInit(UART_HandleTypeDef *huart);
 void Error_handler(void);
 
 UART_HandleTypeDef huart2;
@@ -49,4 +50,28 @@ void Error_handler(void)
 {
 	while(1);
 }
+
+void HAL_UART_MspInit(UART_HandleTypeDef *huart)
+{
+	//Here we are going to do low level initialization of USART2 peripheral
+	GPIO_InitTypeDef gpio_uart2;
+	//1. Enable the clock for USART2 peripheral
+	__HAL_RCC_USART2_CLK_ENABLE();
+	//2. Do the pin muxing configurations
+	gpio_uart2.Mode = GPIO_MODE_AF_PP;
+	gpio_uart2.Pull = GPIO_PULLUP;
+	gpio_uart2.Speed = GPIO_SPEED_LOW;
+	gpio_uart2.Alternate = GPIO_AF7_USART2;
+	gpio_uart2.Pin = GPIO_PIN_2;
+	HAL_GPIO_Init(GPIOA, &gpio_uart2);	//UART2_TX
+	gpio_uart2.Pin = GPIO_PIN_3;
+	HAL_GPIO_Init(GPIOA, &gpio_uart2);	//UART2_RX
+
+	//3. Enable the IRQ and set up the priority (NVIC settings)
+	HAL_NVIC_EnableIRQ(USART2_IRQn);
+	HAL_NVIC_SetPriority(USART2_IRQn, 15, 0);
+}
+
+
+
 
